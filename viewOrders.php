324cb1a86f95ab153b825d1e51a7172f7dd6d9b8
Haooo23 <100354242+Haooo23@users.noreply.github.com>
2024-05-11 17:49:34 +0200

    update

--- viewOrders.php
+++ viewOrders.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
     die("Errore di connessione al database: " . $conn->connect_error);
 }
 
-$sql = "SELECT ordini.id AS ordine_id, ordini.id_utente, utenti.nome AS nome_utente, panini.nome AS panino, panini.descrizione AS descrizione_panino, panini.costo AS costo_panino, bevande.nome AS bevanda, bevande.descrizione AS descrizione_bevanda
+$sql = "SELECT ordini.id AS ordine_id, ordini.id_utente, utenti.nome AS nome_utente, utenti.cognome AS cognome_utente, panini.nome AS panino, panini.descrizione AS descrizione_panino, panini.costo AS costo_panino, bevande.nome AS bevanda, bevande.descrizione AS descrizione_bevanda, bevande.costo AS costo_bevanda
         FROM ordini
         INNER JOIN utenti ON ordini.id_utente = utenti.ID
         LEFT JOIN panini_ordinati ON ordini.id = panini_ordinati.id_ordine
@@ -27,11 +27,13 @@ if ($result->num_rows > 0) {
                 <th>ID Ordine</th>
                 <th>ID Utente</th>
                 <th>Nome Utente</th>
+                <th>Cognome Utente</th>
                 <th>Panino</th>
                 <th>Descrizione Panino</th>
                 <th>Costo Panino</th>
                 <th>Bevanda</th>
                 <th>Descrizione Bevanda</th>
+                <th>Costo Bevanda</th>
                 <th>Azioni</th> <!-- New table header for actions -->
             </tr>";
     while ($row = $result->fetch_assoc()) {
@@ -39,12 +41,14 @@ if ($result->num_rows > 0) {
                 <td>" . $row["ordine_id"] . "</td>
                 <td>" . $row["id_utente"] . "</td>
                 <td>" . $row["nome_utente"] . "</td>
+                <td>" . $row["cognome_utente"] . "</td>
                 <td>" . $row["panino"] . "</td>
                 <td>" . $row["descrizione_panino"] . "</td>
                 <td>" . $row["costo_panino"] . "</td>
                 <td>" . $row["bevanda"] . "</td>
                 <td>" . $row["descrizione_bevanda"] . "</td>
-                <td><button onclick='delete(" . $row["ordine_id"] . ")'>Delete</button></td> 
+                <td>" . $row["costo_bevanda"] . "</td>
+                <td><button onclick='deleteOrder(" . $row["ordine_id"] . ")'>Delete</button></td> <!-- Button for deleting row -->
             </tr>";
     }
     echo "</table>";
@@ -56,9 +60,8 @@ $conn->close();
 ?>
 
 <script>
-    function delete(ordineId) {
+    function deleteOrder(ordineId) {
         if (confirm("Sei sicuro di voler eliminare questo ordine?")) {
-        
         }
     }
 </script>
