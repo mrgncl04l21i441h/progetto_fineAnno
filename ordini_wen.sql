099141da0dc5aaf17f6e17f17a03f09641871be3
Haooo23 <100354242+Haooo23@users.noreply.github.com>
2024-05-19 22:20:56 +0200

    Update ordini_wen.sql

--- ordini_wen.sql
+++ ordini_wen.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: May 19, 2024 at 09:13 AM
--- Server version: 5.7.24
--- PHP Version: 8.0.1
+-- Host: localhost
+-- Creato il: Mag 19, 2024 alle 22:20
+-- Versione del server: 10.4.28-MariaDB
+-- Versione PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bevande`
+-- Struttura della tabella `bevande`
 --
 
 CREATE TABLE `bevande` (
@@ -32,60 +32,66 @@ CREATE TABLE `bevande` (
   `nome` varchar(30) NOT NULL COMMENT 'nome della bevanda',
   `costo` decimal(3,0) NOT NULL COMMENT 'costo della bevanda',
   `descrizione` varchar(200) NOT NULL COMMENT 'descrizione della bevanda (ingredienti, ecc...)'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `bevande`
+-- Dump dei dati per la tabella `bevande`
 --
 
 INSERT INTO `bevande` (`id`, `nome`, `costo`, `descrizione`) VALUES
-(1, 'Acqua minerale', '1', 'Bottiglia d\'acqua minerale naturale'),
-(2, 'Coca-Cola', '2', 'Bottiglia di Coca-Cola 33cl');
+(1, 'Acqua minerale', 1, 'Bottiglia d\'acqua minerale naturale'),
+(2, 'Coca-Cola', 2, 'Bottiglia di Coca-Cola 33cl'),
+(3, 'Fanta', 2, 'Bottiglia di Fanta 33cl'),
+(4, 'Sprite', 2, 'Bottiglia di Sprite 33cl'),
+(5, 'Tè freddo', 2, 'Bottiglia di tè freddo al limone 33cl'),
+(6, 'Caffè', 1, 'Tazza di caffè espresso');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bevande_ordinate`
+-- Struttura della tabella `bevande_ordinate`
 --
 
 CREATE TABLE `bevande_ordinate` (
   `id` int(11) NOT NULL COMMENT 'Codice univoco per identificare l''entità',
   `id_ordine` int(11) NOT NULL COMMENT 'ordine a cui fa riferimento questa entità',
   `id_bevanda` int(11) NOT NULL COMMENT 'bevanda a cui questa entità fa riferimento',
-  `quantità` smallint(6) NOT NULL DEFAULT '1' COMMENT 'numero della bibita ordinato'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `quantità` smallint(6) NOT NULL DEFAULT 1 COMMENT 'numero della bibita ordinato'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `bevande_ordinate`
+-- Dump dei dati per la tabella `bevande_ordinate`
 --
 
 INSERT INTO `bevande_ordinate` (`id`, `id_ordine`, `id_bevanda`, `quantità`) VALUES
-(1, 1, 1, 3);
+(1, 1, 1, 3),
+(6, 4, 3, 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ordini`
+-- Struttura della tabella `ordini`
 --
 
 CREATE TABLE `ordini` (
   `id` int(11) NOT NULL COMMENT 'codice univoco per identificare l''entità',
   `id_utente` int(11) NOT NULL COMMENT 'id dell''utente che ha effettuato la prenotazione',
   `ora_ritiro` datetime NOT NULL COMMENT 'data e orario nel quale si vogliono ritirare i panini',
-  `ora_prenotazione` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'ora in cui è stato effettuato l''ordine'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `ora_prenotazione` datetime DEFAULT current_timestamp() COMMENT 'ora in cui è stato effettuato l''ordine'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `ordini`
+-- Dump dei dati per la tabella `ordini`
 --
 
 INSERT INTO `ordini` (`id`, `id_utente`, `ora_ritiro`, `ora_prenotazione`) VALUES
-(1, 1, '2024-04-22 12:00:00', '2024-04-22 11:30:00');
+(1, 1, '2024-04-22 12:00:00', '2024-04-22 11:30:00'),
+(4, 2, '2024-05-19 20:10:00', '2024-05-19 20:10:50');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `panini`
+-- Struttura della tabella `panini`
 --
 
 CREATE TABLE `panini` (
@@ -93,20 +99,24 @@ CREATE TABLE `panini` (
   `nome` varchar(200) NOT NULL COMMENT 'nome del panino',
   `costo` decimal(3,0) NOT NULL COMMENT 'costo del panino',
   `descrizione` varchar(500) NOT NULL COMMENT 'descrizione del panino e ingredienti'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `panini`
+-- Dump dei dati per la tabella `panini`
 --
 
 INSERT INTO `panini` (`id`, `nome`, `costo`, `descrizione`) VALUES
-(1, 'Panino al salame', '5', 'Panino con salame, formaggio, lattuga e pomodoro'),
-(2, 'Panino vegetariano', '4', 'Panino con verdure grigliate, formaggio e salsa al pesto');
+(1, 'Panino al salame', 5, 'Panino con salame, formaggio, lattuga e pomodoro'),
+(2, 'Panino vegetariano', 4, 'Panino con verdure grigliate, formaggio e salsa al pesto'),
+(3, 'Panino con prosciutto', 5, 'Panino con prosciutto cotto, formaggio, lattuga e maionese'),
+(4, 'Panino al pollo', 6, 'Panino con pollo grigliato, lattuga, pomodoro e salsa Caesar'),
+(5, 'Panino al tonno', 5, 'Panino con tonno, lattuga, pomodoro e maionese'),
+(6, 'Panino vegano', 4, 'Panino con hummus, avocado, lattuga, pomodoro e cetriolo');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `panini_ordinati`
+-- Struttura della tabella `panini_ordinati`
 --
 
 CREATE TABLE `panini_ordinati` (
@@ -114,24 +124,25 @@ CREATE TABLE `panini_ordinati` (
   `id_ordine` int(11) NOT NULL,
   `id_panino` int(11) NOT NULL,
   `quantità` smallint(6) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `panini_ordinati`
+-- Dump dei dati per la tabella `panini_ordinati`
 --
 
 INSERT INTO `panini_ordinati` (`id`, `id_ordine`, `id_panino`, `quantità`) VALUES
-(1, 1, 1, 2);
+(1, 1, 1, 2),
+(7, 4, 3, 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `utenti`
+-- Struttura della tabella `utenti`
 --
 
 CREATE TABLE `utenti` (
   `ID` int(11) NOT NULL COMMENT 'Codice univoco per identificare l''entità',
-  `admin` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'se e'' true l''utente e'' un admin',
+  `admin` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'se e'' true l''utente e'' un admin',
   `nome` varchar(20) NOT NULL COMMENT 'nome dell''utente',
   `cognome` varchar(20) NOT NULL COMMENT 'cognome dell''utente',
   `data_nascita` date DEFAULT NULL COMMENT 'data nascita dell''utente',
@@ -139,11 +150,11 @@ CREATE TABLE `utenti` (
   `email` varchar(100) NOT NULL COMMENT 'email associata all''account',
   `username` varchar(50) NOT NULL COMMENT 'nome utente associto all''account',
   `password` varchar(50) NOT NULL COMMENT 'password associata all''account',
-  `data_iscrizione` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'data e ora dell''iscrizione dell''utente'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `data_iscrizione` datetime DEFAULT current_timestamp() COMMENT 'data e ora dell''iscrizione dell''utente'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `utenti`
+-- Dump dei dati per la tabella `utenti`
 --
 
 INSERT INTO `utenti` (`ID`, `admin`, `nome`, `cognome`, `data_nascita`, `telefono`, `email`, `username`, `password`, `data_iscrizione`) VALUES
@@ -152,81 +163,81 @@ INSERT INTO `utenti` (`ID`, `admin`, `nome`, `cognome`, `data_nascita`, `telefon
 (3, 1, 'Nicolo\'', 'Mereghetti', '2004-07-21', '3315696117', '072004.nick@gmail.com', 'Nicomere', '12345678', '2024-05-19 11:08:40');
 
 --
--- Indexes for dumped tables
+-- Indici per le tabelle scaricate
 --
 
 --
--- Indexes for table `bevande`
+-- Indici per le tabelle `bevande`
 --
 ALTER TABLE `bevande`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `bevande_ordinate`
+-- Indici per le tabelle `bevande_ordinate`
 --
 ALTER TABLE `bevande_ordinate`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `ordini`
+-- Indici per le tabelle `ordini`
 --
 ALTER TABLE `ordini`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `panini`
+-- Indici per le tabelle `panini`
 --
 ALTER TABLE `panini`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `panini_ordinati`
+-- Indici per le tabelle `panini_ordinati`
 --
 ALTER TABLE `panini_ordinati`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `utenti`
+-- Indici per le tabelle `utenti`
 --
 ALTER TABLE `utenti`
   ADD PRIMARY KEY (`ID`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT per le tabelle scaricate
 --
 
 --
--- AUTO_INCREMENT for table `bevande`
+-- AUTO_INCREMENT per la tabella `bevande`
 --
 ALTER TABLE `bevande`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Codice univoco per identificare l''entità', AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Codice univoco per identificare l''entità', AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `bevande_ordinate`
+-- AUTO_INCREMENT per la tabella `bevande_ordinate`
 --
 ALTER TABLE `bevande_ordinate`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Codice univoco per identificare l''entità', AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Codice univoco per identificare l''entità', AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `ordini`
+-- AUTO_INCREMENT per la tabella `ordini`
 --
 ALTER TABLE `ordini`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'codice univoco per identificare l''entità', AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'codice univoco per identificare l''entità', AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `panini`
+-- AUTO_INCREMENT per la tabella `panini`
 --
 ALTER TABLE `panini`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Codice univoco per identificare l''entità', AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Codice univoco per identificare l''entità', AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `panini_ordinati`
+-- AUTO_INCREMENT per la tabella `panini_ordinati`
 --
 ALTER TABLE `panini_ordinati`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Codice univoco per identificare l''entità', AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Codice univoco per identificare l''entità', AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `utenti`
+-- AUTO_INCREMENT per la tabella `utenti`
 --
 ALTER TABLE `utenti`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Codice univoco per identificare l''entità', AUTO_INCREMENT=4;
